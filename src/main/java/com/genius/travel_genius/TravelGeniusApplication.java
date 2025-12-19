66530ddf54ca47df9ef927d3738931dbccd79f1c
Varsha Gupta <varshagupta2105@gmail.com>
2025-12-19 20:07:36 +0530

    Travel Genius Project Setup

---
+++ src/main/java/com/genius/travel_genius/TravelGeniusApplication.java
@@ -0,0 +1,13 @@
+package com.genius.travel_genius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelGeniusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelGeniusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/genius/travel_genius/TravelGeniusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genius.travel_genius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelGeniusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
