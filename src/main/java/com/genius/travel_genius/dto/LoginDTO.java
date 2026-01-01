32ab0de02aba7f3336786959a07a9e4a9d88b73d
Varshagupta143 <varshagupta2105@gmail.com>
2026-01-01 20:46:22 +0530

    Changes in login code

--- src/main/java/com/genius/travel_genius/dto/LoginDTO.java
+++ src/main/java/com/genius/travel_genius/dto/LoginDTO.java
@@ -1,7 +1,9 @@
 package com.genius.travel_genius.dto;
 
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,9 +15,17 @@ import lombok.Setter;
 @NoArgsConstructor
 public class LoginDTO {
 
-    @NotBlank(message = "Email is required")
+
     @Email
     private String email;
+    @Size(min = 10, max = 10, message = "Mobile number must be exactly 10 digits")
+    private String mobileNumber;
     @NotBlank(message = "Password is required")
     private String password;
+    @AssertTrue(message = "Either email or mobile number must be provided")
+    public boolean isEmailOrMobilePresent() {
+        return (email != null && !email.isBlank()) ||
+                (mobileNumber != null && !mobileNumber.isBlank());
+    }
+      
 }
