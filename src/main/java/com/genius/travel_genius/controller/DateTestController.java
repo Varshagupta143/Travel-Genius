3ea031f23a5c58d82a300f35787629ba9786b194
Varshagupta143 <varshagupta2105@gmail.com>
2026-01-05 10:42:16 +0530

    for testing date input

---
+++ src/main/java/com/genius/travel_genius/controller/DateTestController.java
@@ -0,0 +1,19 @@
+package com.genius.travel_genius.controller;
+
+import com.genius.travel_genius.dto.TestDateDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DateTestController {
+    @PostMapping("login")   // using same mapping style
+    public ResponseEntity<String> login(@Valid @RequestBody TestDateDTO testDateDTO) {
+        String result = "Received date: " + testDateDTO.getDob();
+        return ResponseEntity.ok(result);
+    }
+}
---
+++ src/main/java/com/genius/travel_genius/dto/TestDateDTO.java
@@ -0,0 +1,20 @@
+package com.genius.travel_genius.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TestDateDTO {
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dob;
+}
